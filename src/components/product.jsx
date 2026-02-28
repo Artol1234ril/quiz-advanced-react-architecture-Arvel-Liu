889ce3a07a58ea329790052db7e12629bcd11d0e
Artol1234ril <arrrliulah@gmail.com>
2026-02-28 13:06:59 +0700

    first commit

--- src/components/product.jsx
+++ src/components/product.jsx
@@ -4,6 +4,13 @@ import { useNavigate } from 'react-router-dom'
 import Navbar from './navbar'
 import '../Style/product.css'
 
+
+import image1 from '../assets/image1.png'
+import image2 from '../assets/image2.png'
+import image3 from '../assets/image3.png'
+import image4 from '../assets/image4.png'
+import image5 from '../assets/image5.png'
+
 const products = [
   {
     id: 1,
